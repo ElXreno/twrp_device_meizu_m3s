230abf36465f554408402655dc8139026b38aa3c
ElXreno <elxreno@gmail.com>
2018-12-11 18:33:37 +0000

    Exclude SuperSU again Disabled battery percent Disabled reboot to bootloader Update crypto path (but not enabled)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,22 +46,24 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Encryption
 # TW_INCLUDE_CRYPTO := true
 # TW_CRYPTO_FS_TYPE := "f2fs"
-# TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/userdata"
+# TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/by-name/userdata"
 # TW_CRYPTO_MNT_POINT := "/data"
 # TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,inline_data,inline_xattr,data=ordered"
 
 # App
 TW_EXCLUDE_TWRPAPP := true
-TW_EXCLUDE_SUPERSU := false
+TW_EXCLUDE_SUPERSU := true
 
 # Recovery
 TW_SCREEN_BLANK_ON_BOOT := true
+TW_NO_REBOOT_BOOTLOADER := true
 TW_THEME := portrait_hdpi
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/recovery.fstab
 RECOVERY_SDCARD_ON_DATA := true
 TW_DEVICE_VERSION := by ElXreno (Mr_Bagger)
-TW_CUSTOM_BATTERY_PATH := /sys/devices/platform/battery/power_supply/battery/capacity
+TW_NO_BATT_PERCENT := true
+# TW_CUSTOM_BATTERY_PATH := /sys/devices/platform/battery/power_supply/battery/capacity # Doesn't work properly
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
