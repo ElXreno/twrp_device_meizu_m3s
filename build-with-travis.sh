29011f125a09ff7efaa9a36dffd3cbd011bdc047
ElXreno <elxreno@gmail.com>
2019-03-27 11:58:47 +0300

    update build-with-travis.sh

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -1,4 +1,4 @@
-git clone https://gitlab.com/ElXreno/android-5.1-src /build
+git clone https://gitlab.com/ElXreno/android-5.1-src /build --depth=1
 cd /build
 mkdir -p device/meizu/m3s && cp -r /sources/* device/meizu/m3s/
 source build/envsetup.sh && lunch omni_m3s-eng && make -j5 recoveryimage
