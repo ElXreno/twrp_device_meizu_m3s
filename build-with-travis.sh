fc16e1be92b69acb6912cc6cb0e5aadea4a11755
ElXreno <elxreno@gmail.com>
2019-05-24 21:51:40 +0300

    Update build-with-travis.sh

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -1,5 +1,6 @@
-git clone https://gitlab.com/ElXreno/android-5.1-src /build --depth=1
-cd /build
+mkdir /build && cd /build
+repo init --depth=1 -u git://github.com/ElXreno/platform_manifest_twrp_omni.git -b twrp-5.1 && repo sync
+rm -rf .repo && find . -type d -name ".git" -exec rm -rf {} \;
 mkdir -p device/meizu/m3s && cp -r /sources/* device/meizu/m3s/
 source build/envsetup.sh && lunch omni_m3s-eng && make -j5 recoveryimage
 cp /build/out/target/product/m3s/recovery.img /sources/recovery.img
