77aea441c9251e0d8e502b20ff25cbef0247710d
ElXreno <elxreno@gmail.com>
2019-05-24 21:58:30 +0300

    Update build-with-travis.sh

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -1,6 +1,6 @@
 mkdir /build && cd /build
 repo init --depth=1 -u git://github.com/ElXreno/platform_manifest_twrp_omni.git -b twrp-5.1 && repo sync
-rm -rf .repo && find . -type d -name ".git" -exec rm -rf {} \;
+rm -rf .repo && find . -type d -name ".git" -execdir rm -rf {} \;
 mkdir -p device/meizu/m3s && cp -r /sources/* device/meizu/m3s/
 source build/envsetup.sh && lunch omni_m3s-eng && make -j5 recoveryimage
 cp /build/out/target/product/m3s/recovery.img /sources/recovery.img
