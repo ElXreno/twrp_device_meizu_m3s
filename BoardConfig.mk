9ec9767d361ca99c8066349a924e58518934a6a2
ElXreno <elxreno@gmail.com>
2019-06-02 14:19:18 +0300

    Disabled haptic feedback

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ TW_THEME := portrait_hdpi
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_REBOOT_BOOTLOADER := true
+TW_NO_HAPTICS := true
 TW_DEVICE_VERSION := by ElXreno (v0.2.1-alpha)
 
 # TWRP excludes
