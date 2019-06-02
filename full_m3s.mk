92b5aa2e93943d7cff858953ad76660216af64d5
ElXreno <elxreno@gmail.com>
2019-06-02 13:42:29 +0300

    Update

--- full_m3s.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := m3s
-PRODUCT_NAME := full_m3s
-PRODUCT_BRAND := meizu
-PRODUCT_MODEL := m3s
-PRODUCT_MANUFACTURER := meizu
-PRODUCT_BOARD := m3s
--- omni_m3s.mk
+++ omni_m3s.mk
@@ -1,9 +1,23 @@
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-$(call inherit-product, device/meizu/m3s/full_m3s.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_PACKAGES += \
     charger_res_images \
     charger
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.secure=0 \
+    ro.adb.secure=0 \
+    security.perf_harden=0 \
+    ro.allow.mock.location=1
+
+PRODUCT_MANUFACTURER := Meizu
+
+PRODUCT_DEVICE := m3s
 PRODUCT_NAME := omni_m3s
+PRODUCT_BRAND := Meizu
+PRODUCT_MODEL := M3s
+PRODUCT_BOARD := m3s
