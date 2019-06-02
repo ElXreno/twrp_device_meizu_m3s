54284c7aedc2ff092b757d9a6f8abd3a6d7b9832
ElXreno <40758597+ElXreno@users.noreply.github.com>
2019-06-02 13:37:29 +0300

    Update omni_m3s.mk

--- omni_m3s.mk
+++ omni_m3s.mk
@@ -2,4 +2,8 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, device/meizu/m3s/full_m3s.mk)
 
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    charger
+
 PRODUCT_NAME := omni_m3s
