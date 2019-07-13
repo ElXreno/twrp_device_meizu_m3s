42eacbfd83fda76c0b5d9820adea4d7d2d4c2871
ElXreno <40758597+ElXreno@users.noreply.github.com>
2019-07-13 20:37:42 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brigh
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_NO_HAPTICS := true
-TW_DEVICE_VERSION := by ElXreno (v0.2.2-alpha)
+TW_DEVICE_VERSION := by ElXreno (v0.2.3-alpha)
 TW_EXTRA_LANGUAGES := true
 TW_BUILD_ZH_CN_SUPPORT := true
 
