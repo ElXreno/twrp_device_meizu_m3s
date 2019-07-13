9464c49d60a53883f431db5a36f97c6c1c8a5230
ElXreno <elxreno@gmail.com>
2019-07-13 19:30:54 +0300

    Update Travis CI build script

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -1,6 +1,12 @@
 mkdir /build && cd /build
-repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-5.1 && repo sync
+
+repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-6.0 
+repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
+
 rm -rf .repo && find . -type d -name ".git" -execdir rm -rf {} \;
+
 mkdir -p device/meizu/m3s && cp -r /sources/* device/meizu/m3s/
+
 source build/envsetup.sh && lunch omni_m3s-eng && make -j5 recoveryimage
+
 cp /build/out/target/product/m3s/recovery.img /sources/recovery.img
