5f899f7945f620be44bdd435441c4abc836d9d67
ElXreno <elxreno@gmail.com>
2019-07-13 19:38:14 +0300

    Little re-indent and core count placer

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -1,10 +1,13 @@
 mkdir /build && cd /build
 
 repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-6.0 
-repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
+repo sync -c -j$(nproc) --force-sync --no-clone-bundle --no-tags
 
-mkdir -p device/meizu/m3s && cp -r /sources/* device/meizu/m3s/
+mkdir -p device/meizu/m3s 
+cp -r /sources/* device/meizu/m3s/
 
-source build/envsetup.sh && lunch omni_m3s-eng && make -j5 recoveryimage
+source build/envsetup.sh 
+lunch omni_m3s-eng 
+make -j$(nproc) recoveryimage
 
 cp /build/out/target/product/m3s/recovery.img /sources/recovery.img
