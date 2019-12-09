d3a12fc670a3c756f5ce16bc4f7ed1b5a1d2dd99
ElXreno <elxreno@gmail.com>
2019-12-09 21:12:11 +0300

    Undockerify

--- build-with-travis.sh
+++
@@ -1,6 +0,0 @@
-mkdir /build && cd /build
-repo init --depth=1 -u git://github.com/ElXreno/platform_manifest_twrp_omni.git -b twrp-5.1 && repo sync --no-clone-bundle --no-tags
-rm -rf .repo && find . -type d -name ".git" -execdir rm -rf {} \;
-mkdir -p device/meizu/m3s && cp -r /sources/* device/meizu/m3s/
-source build/envsetup.sh && lunch omni_m3s-eng && make -j5 recoveryimage
-cp /build/out/target/product/m3s/recovery.img /sources/recovery.img
